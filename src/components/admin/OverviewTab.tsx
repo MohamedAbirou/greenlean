613e5d89f61e7d6892247ece2c440badf0568d05
Mohamed Abirou <109366637+MohamedAbirou@users.noreply.github.com>
2025-10-10 23:41:13 +0100

    Update OverviewTab.tsx

--- src/components/admin/OverviewTab.tsx
+++ src/components/admin/OverviewTab.tsx
@@ -2,7 +2,8 @@ import Chart from 'chart.js/auto';
 import { Award, Star, TrendingUp, Users } from 'lucide-react';
 import React, { useEffect, useRef, useState } from 'react';
 import { supabase } from '../../lib/supabase';
-import { StatCard, ChartCard } from "../../helpers"
+import StatCard from '../helpers/StatCard';
+import ChartCard from '../helpers/ChartCard';
 
 interface DashboardStats {
   totalParticipants: number;
